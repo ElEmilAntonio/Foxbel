c99f88f38a91590dacf7e6a5febb116d0f248797
gamaliel <gama.amc@gmail.com>
2021-07-04 18:11:17 -0500

    Menu show/hide on screen size -u

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,34 @@
 import '../css/index.css';
 import { initializeAPI, showLogin as login, logout } from "./api_deezer";
 
-window.showLogin = () =>{
-    login();
-}
+window.showLogin = () => login()
 
-window.logout = () => {
-    logout();
-}
+window.logout = () => logout()
+
+window.showMenu = () => showMenu()
+
+window.closeMenu = () => closeMenu()
 
 initializeAPI();
+
+//Emil responsive menu xD
+const showMenu = () => document.getElementById("menu").style.display = 'flex'
+
+/*
+$('#boton-menu').click(function(){
+    $('.menu-container').css("display","flex");	
+    });	
+    */
+    /*
+    $('#boton-cerrar-menu').click(function(){
+        $('.menu-container').css("display","none");	
+    });
+    */
+const closeMenu = () => document.getElementById("menu").style.display = 'none'
+    /*
+    $(document).ready(function(){
+      $(window).resize(function(){
+       if($(window).width()>=768)  $('.menu-container').css("display","flex");	
+      });
+    });
+    */
