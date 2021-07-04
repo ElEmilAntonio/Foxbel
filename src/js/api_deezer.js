93b8fdb6e92c76240e6b692ba9688ae950f8634f
gamaliel <gama.amc@gmail.com>
2021-07-04 02:44:16 -0500

    User login/logout, localstorage session -a

--- src/js/api_deezer.js
+++ src/js/api_deezer.js
@@ -1,7 +1,13 @@
 const appID = '489402';
-const appSecretKey = 'a5775013018f7dc67e119742b3b471ee';
-let accessToken = 'frCpv4T49lhSmMVBQWL76pbq38M99gWEy7w99euk80rgU4MVw7';
 const channelURL = 'http://localhost/foxbel-music/dist/channel.html';
+let user = {
+    id: localStorage.getItem("userId"),
+    name: localStorage.getItem("userName"),
+    accessToken: localStorage.getItem("userToken"),
+    status: localStorage.getItem("userStatus")
+};
+
+const appSecretKey = 'a5775013018f7dc67e119742b3b471ee';
 
 const initializeAPI = () => {
     DZ.init({
@@ -11,24 +17,71 @@ const initializeAPI = () => {
     getStatus();
 }
 
-const getStatus = () => {
+const getStatus = () => { 
+    if(user.status == "connected"){  
+        let logoutIcon = document.getElementById("logoutIcon");
+        let userName = document.getElementById("userName");
+        logoutIcon.classList.remove("is-hidden");
+        userName.innerHTML = user.name;
+        userName.onclick = null;
+        userName.classList.remove("login-hover");      
+        getChart();   
+    }else{
+        alert("Debe identificarse con Deezer para usar Foxbel Music");
+    }
+    /*
     DZ.getLoginStatus(res => {
         res.status == 'connected' ? setUserInfo() : alert(res.status);
     });
+    */
+}
+
+const logout = () => {
+    if(confirm("Deseas cerrar sesión?")){
+        updateUserData(null, null, null, 'not_authorized');
+        DZ.logout();
+    }
 }
 
 const showLogin = () =>{
     DZ.login(response => {
-        response.status == 'connected' ? setUserInfo() : getStatus()
+        response.status == 'connected' ? login(response) : getStatus()
     }, {perms: 'basic_access, email, offline_access'});
 }
 
-const setUserInfo = () => {
+const login = (data) => {
     DZ.api('/user/me', res => {
-        let userName = document.getElementById("userName");
-        userName.innerHTML = res.name;
+        updateUserData(data.userID, res.name, data.authResponse.accessToken, data.status);
     });    
     getChart();   
+}
+
+const updateUserData = (id, name, token, status) => {  
+    let logoutIcon = document.getElementById("logoutIcon");
+    let userName = document.getElementById("userName");
+    if(status == 'connected'){
+        localStorage.setItem("userId", id);
+        localStorage.setItem("userName",name);
+        localStorage.setItem("userToken",token);
+        localStorage.setItem("userStatus", status);
+        logoutIcon.classList.remove("is-hidden");
+        userName.innerHTML = name;
+        userName.onclick = null;
+        userName.classList.remove("login-hover");
+    }else{
+        localStorage.removeItem("userId");
+        localStorage.removeItem("userName");
+        localStorage.removeItem("userToken");
+        localStorage.removeItem("userStatus");
+        logoutIcon.classList.add("is-hidden");
+        userName.innerHTML = "Inicia sesión";
+        userName.onclick = () => showLogin();
+        userName.classList.add("login-hover");        
+    }
+    user.id = localStorage.getItem("userId");
+    user.name = localStorage.getItem("userName");
+    user.accessToken = localStorage.getItem("userToken");
+    user.status = localStorage.getItem("userStatus");
 }
 
 const getChart = () => {
@@ -45,7 +98,9 @@ const getChart = () => {
         data.shift();
         //Resultados
         const resultsRowOne = document.getElementById("first-row");
+        resultsRowOne.innerHTML = "";
         const resultsRowTwo = document.getElementById("second-row");
+        resultsRowTwo .innerHTML = "";
         data.map((song, index) => {
             index < 5 ? addToRow(resultsRowOne, song) : addToRow(resultsRowTwo, song)
         })
@@ -101,4 +156,4 @@ const addToRow = (row, song) => {
 
 
 
-export {initializeAPI, showLogin};
+export {initializeAPI, showLogin, logout};
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import '../css/index.css';
-import { initializeAPI, showLogin as login } from "./api_deezer";
+import { initializeAPI, showLogin as login, logout } from "./api_deezer";
 
 window.showLogin = () =>{
     login();
+}
+
+window.logout = () => {
+    logout();
 }
 
 initializeAPI();
