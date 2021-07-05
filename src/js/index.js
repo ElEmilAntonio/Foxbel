4a3611fb0264bce1079b6317ddef2c780895b52f
gamaliel <gama.amc@gmail.com>
2021-07-05 16:33:48 -0500

    Menu hide/show effect, overlay menu fixed -u

--- src/js/index.js
+++ src/js/index.js
@@ -12,23 +12,6 @@ window.closeMenu = () => closeMenu()
 initializeAPI();
 
 //Emil responsive menu xD
-const showMenu = () => document.getElementById("menu").style.display = 'flex'
+const showMenu = () => document.getElementById("menu").classList.remove('hide-menu')
 
-/*
-$('#boton-menu').click(function(){
-    $('.menu-container').css("display","flex");	
-    });	
-    */
-    /*
-    $('#boton-cerrar-menu').click(function(){
-        $('.menu-container').css("display","none");	
-    });
-    */
-const closeMenu = () => document.getElementById("menu").style.display = 'none'
-    /*
-    $(document).ready(function(){
-      $(window).resize(function(){
-       if($(window).width()>=768)  $('.menu-container').css("display","flex");	
-      });
-    });
-    */
+const closeMenu = () => document.getElementById("menu").classList.add('hide-menu')
